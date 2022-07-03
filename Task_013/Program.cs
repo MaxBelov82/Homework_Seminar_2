eb78030121aba59a4aa7d184556697ca0d5068cb
Maxim Belov <belov.max7@yandex.ru>
2022-07-03 19:28:34 +0300

    Отформатировал текст задачи

--- Task_013/Program.cs
+++ Task_013/Program.cs
@@ -5,13 +5,13 @@
 // 32679 -> 6
 
 Console.WriteLine("Введите число :");
-int i =Convert.ToInt32(Console.ReadLine());
+int i = Convert.ToInt32(Console.ReadLine());
 
-if (i<=99)
-Console.WriteLine("Нет третьего числа");
+if (i <= 99)
+    Console.WriteLine("Нет третьего числа");
 
-else if (i<=999)
- Console.WriteLine(i%10);
- 
+else if (i <= 999)
+    Console.WriteLine(i % 10);
+
 else if (i <= 99999)
     Console.WriteLine((i / 100) % 10);
